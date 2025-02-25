18f36465d3716f290e466457d53690d942a7de3a
nazrulislam01865 <nazrul01865@gmail.com>
2025-02-25 21:52:07 +0600

    loginScreen and ui helper

---
+++ lib/domain/constant/appColors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+class Appcolors {
+  static const Color ScaffoldBackground = Color(0xFFE5E5E5);
+}
---
+++ lib/repository/widgets/UiHelper.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Uihelper {
+  static CustomImage({required String image}) {
+    return Image.asset("assets/images/$image");
+  }
+}
